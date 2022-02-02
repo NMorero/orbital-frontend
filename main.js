3aef4a7450a5a81dcbcdbc253d557dea4f586e22
Appelie <appelie@MacBook-Air-de-Appelie.local>
2022-02-02 09:42:40 -0300

    S

--- main.js
+++ main.js
@@ -4,43 +4,61 @@ const Client = require('ftp');
 const os = require('os')
 const fs = require('fs');
 const axios = require('axios');
+const checkInternetConnected = require('check-internet-connected');
 
-const DATA_PATH = 'C:/Users/'+os.userInfo().username+'/AppData/Local/Programs/orbitaloffline/DATA';
+
+var isWin = process.platform === "win32";
+let DATA_PATH
+if(isWin){
+  DATA_PATH = 'C:/Users/'+os.userInfo().username+'/AppData/Local/Programs/orbitaloffline/DATA';
+}else{
+  DATA_PATH = '/Users/'+os.userInfo().username+'/Documents/orbitaloffline/DATA';
+}
+
 const JSON_PATH = DATA_PATH+'/json';
 const IMAGES_PATH = DATA_PATH+'/images';
 const ORBS_PATH = DATA_PATH+'/orbs';
 const API_URL = 'http://localhost:3000';
 
-ipcMain.on('test',async (event, arg) => {
-  if (!fs.existsSync(DATA_PATH)){
-    fs.mkdirSync((DATA_PATH));
-    fs.mkdirSync(JSON_PATH, { recursive: true });
-    fs.mkdirSync(IMAGES_PATH, { recursive: true });
-    fs.mkdirSync(ORBS_PATH, { recursive: true });
-
-    axios({
-      method: 'get',
-      url: API_URL+'/getOrbs',
-      responseType: 'stream'
-    })
+ipcMain.on('test', (event, arg) => {
+  checkInternetConnected()
+  .then((result) => {
+    if (!fs.existsSync(DATA_PATH)){
+      if(!isWin){
+        fs.mkdirSync('/Users/'+os.userInfo().username+'/Documents/orbitaloffline');
+      }
+      fs.mkdirSync(DATA_PATH);
+      fs.mkdirSync(JSON_PATH, { recursive: true });
+      fs.mkdirSync(IMAGES_PATH, { recursive: true });
+      fs.mkdirSync(ORBS_PATH, { recursive: true });
+  
+      axios({
+        method: 'get',
+        url: API_URL+'/getOrbs',
+      })
+        .then(function (response) {
+          console.log(response.data.data)
+        });
+  
+    }else{
+      axios({
+        method: 'get',
+        url: API_URL+'/getOrbs',
+      })
       .then(function (response) {
-        console.log(response.data.data)
+        console.log(response.data.orbs[0].id == test.id)
       });
-
-  }else{
-    axios({
-      method: 'get',
-      url: API_URL+'/getOrbs',
-    })
-    .then(function (response) {
-      console.log(response.data.data[0].id == test.id)
+    }
+  })
+  .catch((ex) => {
+    console.log('ex'); // cannot connect to a server or error occurred.
     });
-  }
-})
+  });
 
-require('electron-reload')(__dirname, {
+
+/*require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-});
+}); */
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 500,
